2aed0ee287a57d1711f4bc18e36d65819cd9bef9
fengli <fengliii123@126.com>
2018-03-01 10:56:56 +0800

    layout learn

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,17 @@
 //index.js
 //获取应用实例
-Page({
-  data: {
-    mytext: 'Hello World'
-  }
-})
+var param = {
+    data: {
+        clickMsg: "显示点击的内容"
+    },
+    clickMe: function (e) {
+        console.log(e);
+
+        param.data.clickMsg = "显示点击内容" + "view0";
+        this.setData(param.data);
+    },
+    doLoading: function (e) {
+        console.log(e);
+    }
+}
+Page(param);
