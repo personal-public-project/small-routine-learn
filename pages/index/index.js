8deef9c51a69ae94d7fdccaabfebff38d2534c4c
fengli <fengliii123@126.com>
2018-03-05 11:37:05 +0800

    layout learn

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,18 @@
 //index.js
 //获取应用实例
 var param = {
-    data: {
-        clickMsg: "显示点击的内容"
+    data:{
+        message:"微信小程序",
+        array:[1,2,3,4,"hello","wx"],
+        view:"APP",
+        zhangsan:{name:"张三",age:18},
+        lisi:{name:"李四",age:28},
+        count:0
     },
-    clickMe: function (e) {
-        console.log(e);
-
-        param.data.clickMsg = "显示点击内容" + "view0";
-        this.setData(param.data);
-    },
-    doLoading: function (e) {
-        console.log(e);
+    add:function (e) {
+        this.setData({
+            count:this.data.count+1
+        })
     }
 }
 Page(param);
