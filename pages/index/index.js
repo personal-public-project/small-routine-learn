1b7e475ee1e5343820a87b98e4af30dd1bc66d52
fengli <fengliii123@126.com>
2018-03-05 11:39:55 +0800

    new brh

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,4 @@
 //index.js
 //获取应用实例
-var param = {
-    data:{
-        message:"微信小程序",
-        array:[1,2,3,4,"hello","wx"],
-        view:"APP",
-        zhangsan:{name:"张三",age:18},
-        lisi:{name:"李四",age:28},
-        count:0
-    },
-    add:function (e) {
-        this.setData({
-            count:this.data.count+1
-        })
-    }
-}
+var param = {}
 Page(param);
