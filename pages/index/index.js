1418baa33c8cbd92ef6287b21ef66747f0c7b788
fengli <fengliii123@126.com>
2018-03-09 16:26:25 +0800

    布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,15 @@
-//index.js
-//获取应用实例
-var param = {}
-Page(param);
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      imgUrls:[
+          'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      ],
+      contentItems:[1,2,3,4],
+      listItems:[1,2,3,4]
+  }
+})
